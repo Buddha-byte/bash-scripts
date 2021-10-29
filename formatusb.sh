a01b002b94a3079d182ce8b6bbf693888fd59b5a
PyBuddha <walvater.wotan7@gmail.com>
2021-10-29 23:51:11 +0300

    create formatusb script

--- formatusb.sh
+++ formatusb.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
 
+# My formatting script for usb flash drive
 
+echo "--------------------------------------------"
+echo -e "\t Welcome, my dear friend! "
+echo "--------------------------------------------"
+read -p "Give me the type of filesystem! " FISYS
+read -p "Of course, I need a name of a device! " DEVICE
 
-sudo dd if=/dev/zero of=$1 bs=4096 status=progress
+# Umount target device
+sudo umount $DEVICE &>/dev/null
 
-sudo umount $1
-
-
+if [ "$FISYS" == "vfat" ]; # FAT32 formatting options
+then
+    echo "I'll do it for you!"
+    sudo mkfs.vfat $DEVICE -n sk &>/dev/null
+    if [ "$?" == 0 ];
+    then
+        echo "Well done! Congratulations!"
+    else
+        echo "Oops! Something goes wrong!"
+    fi
+elif [ "$FISYS" == "ntfs" ]; # NTFS formatting optinos
+then
+    echo "I'll do it for you!"
+    sudo mkfs.ntfs $DEVICE &>/dev/null
+    if [ "$?" == 0 ];
+    then
+        echo "Well done! Congratulations!"
+    else
+        echo "Oops! Something goes wrong!"
+    fi
+elif [ "$FISYS" == "ext4" ]; # EXT4 formatting options
+then
+    echo "I'll do it for you!"
+    sudo mkfs.ext4 $DEVICE &>/dev/null
+    if [ "$?" == 0 ];
+    then
+        echo "Well done! Congratulations!"
+    else
+        echo "Oops! Something goes wrong!"
+    fi
+fi
