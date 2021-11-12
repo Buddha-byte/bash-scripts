1e47d431183059371e090e6101b6cec4acdbe705
PyBuddha <walvater.wotan7@gmail.com>
2021-11-12 20:40:56 +0300

    modified playlistmake.sh

--- playlistmake.sh
+++ playlistmake.sh
@@ -4,4 +4,4 @@
 
 DEFAULT="/home/py_buddha/Downloads/Videos/Playlists"
 
-$(which find) $PWD -type f -iname '*.mkv' | sort > $DEFAULT/$1.m3u
+"$(which find)" "$PWD" -type f -iname '*.mkv' | sort > "$DEFAULT"/"$1".m3u
