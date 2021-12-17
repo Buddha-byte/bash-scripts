c1555218c66e052e321ee01688526e5921ddc510
py_buddha <walvater.wotan7@gmail.com>
2021-12-17 19:11:43 +0300

    Improve my encrypter
    Not too much, though :)

--- encrypt.sh
+++ encrypt.sh
@@ -8,14 +8,14 @@ CHOICE="Encrypt Decrypt"
 select option in $CHOICE; do
     if [ $REPLY = 1 ];
     then
-        echo "You want Encrypt"
+        echo "You want encrypt"
         echo "Please, give me a file's name"
         read file;
         gpg -c $file
         echo "The file has been encrypted"
         exit 0
     else
-        echo "You want Decrypt"
+        echo "You want decrypt"
         echo "Please, enter the file's name"
         read file2;
         gpg -d $file2
